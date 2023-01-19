5d4937834bbbbe31702286391cf8ea09ec135a93
Michael Pacheco <git@michaelpacheco.org>
2023-01-19 15:37:45 -0800

    linked_lists:rust:bad_stack: yet another way of rewriting the loop contents.
    * Still have to figure out the type in the match arm, my guess is that since Elem arm is actual values in the enum matches on actual values, clippy with restrictive on sees this and complains. Its not possible to actually the actual values since the values should not be consume. Thankfully rust can acertain what the code wants to do and workaround this.
    * Remade the Head arm to just break from the loop. Much simpler this way.
    
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- rust/bad_stack/src/lib.rs
+++ rust/bad_stack/src/lib.rs
@@ -9,27 +9,26 @@ mod tests {
 
     #[test]
     fn it_works() {
+        let len = 3;
+        let mut loops = 0;
         let list = List::Head(Some(Box::from(List::Elem(32, Some(Box::from(List::Elem(5, Some(Box::from(List::Elem(8, None))))))))));
         // always assume that the first element after creation of our Linked list will be the Head.
-        let mut ptr = if let List::Head(ref elem) = list { elem } else {unreachable!()};
-        loop {
-            ptr = match ptr {
-                &Some(ref wrapped_ptr) => {
-                    let x  = &**wrapped_ptr;
-                    match x {
-                        // clippy tells me that this is the wrong type
-                        List::Elem(val, next) => {
-                            println!("NODE WITH VALUE {val}");
-                            next
-                        },
-                        List::Head(_) => unreachable!()
-                    }
-                },
-                None => {
-                    println!("We are at the end of the line!");
-                    return;
+        let mut ptr = if let List::Head(ref elem) = list { elem.as_ref() } else {unreachable!()};
+        'looping: loop {
+            ptr = if let Some(wrapped_ptr) = ptr {
+                if let List::Elem(val, next) = wrapped_ptr.as_ref() {
+                    println!("NODE WITH VALUE {val}");
+                    loops += 1;
+                    next.as_ref()
+                } else{
+                    // we should never be here, but break just in case;
+                    break 'looping;
                 }
+            } else {
+                println!("We are at the end of the line!");
+                break 'looping;
             };
         }
+        assert_eq!(len, loops, "DID NOT LOOP THROUGH ALL THE VALUES");
     }
 }

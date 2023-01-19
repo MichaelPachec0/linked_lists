4d96f488e87373d4a2ceaa5c0b628a91f7763068
Michael Pacheco <git@michaelpacheco.org>
2023-01-19 15:38:59 -0800

    linked_lists:rust:bad_stack: reformat.
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- rust/bad_stack/src/lib.rs
+++ rust/bad_stack/src/lib.rs
@@ -1,7 +1,7 @@
 #![allow(clippy::missing_docs_in_private_items, clippy::exhaustive_enums)]
 pub enum List {
     Elem(i32, Option<Box<List>>),
-    Head(Option<Box<List>>)
+    Head(Option<Box<List>>),
 }
 #[cfg(test)]
 mod tests {
@@ -11,16 +11,26 @@ mod tests {
     fn it_works() {
         let len = 3;
         let mut loops = 0;
-        let list = List::Head(Some(Box::from(List::Elem(32, Some(Box::from(List::Elem(5, Some(Box::from(List::Elem(8, None))))))))));
+        let list = List::Head(Some(Box::from(List::Elem(
+            32,
+            Some(Box::from(List::Elem(
+                5,
+                Some(Box::from(List::Elem(8, None))),
+            ))),
+        ))));
         // always assume that the first element after creation of our Linked list will be the Head.
-        let mut ptr = if let List::Head(ref elem) = list { elem.as_ref() } else {unreachable!()};
+        let mut ptr = if let List::Head(ref elem) = list {
+            elem.as_ref()
+        } else {
+            unreachable!()
+        };
         'looping: loop {
             ptr = if let Some(wrapped_ptr) = ptr {
                 if let List::Elem(val, next) = wrapped_ptr.as_ref() {
                     println!("NODE WITH VALUE {val}");
                     loops += 1;
                     next.as_ref()
-                } else{
+                } else {
                     // we should never be here, but break just in case;
                     break 'looping;
                 }

e8de73713a8e038f01f7bfdc0e9f7a91f7d3ac86
Michael Pacheco <git@michaelpacheco.org>
2023-01-18 19:41:49 -0800

    linked_lists:rust:bad_stack: Added tests.
    * Still bad stack.
    
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- rust/bad_stack/src/lib.rs
+++ rust/bad_stack/src/lib.rs
@@ -1,6 +1,7 @@
+#![allow(clippy::missing_docs_in_private_items, clippy::exhaustive_enums)]
 pub enum List {
     Elem(i32, Option<Box<List>>),
-    Head(Box<List>)
+    Head(Option<Box<List>>)
 }
 #[cfg(test)]
 mod tests {
@@ -8,6 +9,27 @@ mod tests {
 
     #[test]
     fn it_works() {
-
+        let list = List::Head(Some(Box::from(List::Elem(32, Some(Box::from(List::Elem(5, Some(Box::from(List::Elem(8, None))))))))));
+        // always assume that the first element after creation of our Linked list will be the Head.
+        let mut ptr = if let List::Head(ref elem) = list { elem } else {unreachable!()};
+        loop {
+            ptr = match ptr {
+                &Some(ref wrapped_ptr) => {
+                    let x  = &**wrapped_ptr;
+                    match x {
+                        // clippy tells me that this is the wrong type
+                        List::Elem(val, next) => {
+                            println!("NODE WITH VALUE {val}");
+                            next
+                        },
+                        List::Head(_) => unreachable!()
+                    }
+                },
+                None => {
+                    println!("We are at the end of the line!");
+                    return;
+                }
+            };
+        }
     }
 }

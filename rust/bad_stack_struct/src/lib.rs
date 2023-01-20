057f5f5039a17baf97bdc03b3feb0883b2059b4f
Michael Pacheco <git@michaelpacheco.org>
2023-01-20 10:23:02 -0800

    linked_lists:rust:bad_stack_v2: Add initial split_at.
    * It is still not completely finished, have a off by one error. Still need to fix.
    * Operation is naive and O(n).
    
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- rust/bad_stack_struct/src/lib.rs
+++ rust/bad_stack_struct/src/lib.rs
@@ -1,6 +1,6 @@
 #![allow(clippy::missing_docs_in_private_items)]
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 struct Node {
     value: i32,
     next: Option<Box<Node>>,
@@ -62,6 +62,26 @@ impl Head {
             None
         }
     }
+    pub fn split_at(&mut self, where_at: usize) -> Result<Head, liberr::Err> {
+        let mut loc: usize = 0;
+        // first need to check if it wants the first item;
+        if let Some(mut item) = self.next.as_mut() {
+            'looper: loop {
+                if loc == where_at {
+                    let mut head = Head::new();
+                    head.next = Some(core::mem::take(item));
+                    return Ok(head);
+                } else if let Some(ref mut next) = item.next {
+                    loc += 1;
+                    item = next;
+                } else {
+                    return Err(liberr::Err::new("NO NODE AT LOCATION {where_at} FOUND".to_owned(), line!()));
+                }
+            }
+        } else {
+            Err(liberr::Err::new("NO ITEMS".to_owned(), line!()))
+        }
+    }
 }
 
 #[cfg(test)]
@@ -70,9 +90,9 @@ mod tests {
     const VALS: [i32; 5] = [1, 2, 3, 4, 5];
 
     #[test]
-    fn it_works() {
+    fn it_works() -> Result<(), liberr::Err> {
         let mut head = Head::new();
-        let mut tmp: Option<&Box<Node>> = None;
+        let tmp: Option<&Box<Node>> = None;
         for (i, &value) in VALS.iter().enumerate() {
             head.insert(value);
         }
@@ -82,5 +102,9 @@ mod tests {
         } else {
             println!("Found NOTHING");
         }
+        let new_head = head.split_at(2)?;
+        println!("{new_head:?}");
+        println!("{head:?}");
+        Ok(())
     }
 }

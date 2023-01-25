0bcf963c135f4d63c4cc3ff53e58ba35572fa8de
Michael Pacheco <git@michaelpacheco.org>
2023-01-24 18:09:47 -0800

    linked_lists:rust:bad_stack_v2: add the tests by LRWETMLL.
    * Simple test, but might as well add them to the code.
    
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- rust/bad_stack_struct/src/lib.rs
+++ rust/bad_stack_struct/src/lib.rs
@@ -305,4 +305,32 @@ mod tests {
             "EXPECTED LENGTH {expected_len} DOES NOT EQUAL LIST LENGTH {actual_len}"
         );
     }
+    #[test]
+    fn lrwetmll_initial_tests() {
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop(), None);
+
+        // Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push(4);
+        list.push(5);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
 }
